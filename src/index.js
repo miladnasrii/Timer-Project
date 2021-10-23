39c9cbd1f40fe472e00cdf5b011c57487ace51d9
Milad <miladnasri4030@gmail.com>
2021-10-23 09:11:55 +0330

    first React project with style

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React  from "react";
+import ReactDOM from 'react-dom'
+import './style.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Timer extends React.Component{
+  constructor()
+  {
+    super();
+    this.state = {
+      time : new Date().toLocaleTimeString()
+    }
+  }
+  render(){
+    setInterval(() => {
+      this.setState({
+        time : new Date().toLocaleTimeString()
+      })
+    }, 1000);
+    return (
+      <h2 className="timer">It is {this.state.time}</h2>
+    )
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Hello extends React.Component{
+  render()
+  {
+    return (
+      <h1>Hello My Friends</h1>
+    )
+  }
+}
+
+
+class App extends React.Component{
+  render(){
+    return (
+      <div className="main">
+        <Hello/>
+        <Timer/>
+      </div>
+    )
+  }
+}
+
+// const tick = () =>
+// {
+  ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+  )
+// }
+// setInterval(() => {
+//   tick();
+// }, 1000);
